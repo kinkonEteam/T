91b5075cd90bd77501e7c13a88315c7af3c3f9b9
GA1 <GA1@DEV1-11>
2018-11-27 14:44:24 +0900

    ボスBGM

--- 桃太郎最強伝説/桃太郎最強伝説/SceneMain.cpp
+++ 桃太郎最強伝説/桃太郎最強伝説/SceneMain.cpp
@@ -64,8 +64,8 @@ void CSceneMain::InitScene()
 	Audio::LoadAudio(2, L"アイテムゲット.wav", EFFECT);		//アイテム取得時SE
 	Audio::LoadAudio(3, L"仲間ゲット.wav", EFFECT);			//お供を増やした時用SE
 
-	Audio::LoadAudio(10, L"StairsSE.wav", EFFECT);			//階段用SE
-	Audio::LoadAudio(11, L"ButtonSE.wav", EFFECT);			//インベントリ用SE(予定)															//ボリュームを1.0に戻す
+	Audio::LoadAudio(4, L"StairsSE.wav", EFFECT);			//階段用SE
+	Audio::LoadAudio(5, L"ButtonSE.wav", EFFECT);			//インベントリ用SE(予定)															//ボリュームを1.0に戻す
 	float v = Audio::VolumeMaster(0);
 	v = Audio::VolumeMaster((1.0 - v));
 
