5b58ce3d0ffa24b6130b1205d00b17ef81a43ea8
GA1 <GA1@DEV1-6>
2018-11-29 16:41:35 +0900

    p

--- 桃太郎最強伝説/桃太郎最強伝説/GameHead.h
+++ 桃太郎最強伝説/桃太郎最強伝説/GameHead.h
@@ -148,5 +148,5 @@ enum ITEM
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START  CSceneTitle
+#define SET_GAME_START  CSceneClear
 //-----------------------------------------------
