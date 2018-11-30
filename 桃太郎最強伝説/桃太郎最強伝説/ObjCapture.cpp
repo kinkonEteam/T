97129b6f058b73a6dff64e48ca0aa2cb057f0eb5
たくま <GA1@DEV1-10>
2018-11-30 18:58:21 +0900

    シーン追加

--- 桃太郎最強伝説/桃太郎最強伝説/ObjCapture.cpp
+++ 桃太郎最強伝説/桃太郎最強伝説/ObjCapture.cpp
@@ -3,25 +3,25 @@
 //#include"GameL\WinInputs.h"
 
 #include"GameHead.h"
-#include"ObjYami.h"
+#include"ObjCapture.h"
 
 
 //使用するネームスペース
 using namespace GameL;
 
 //イニシャライズ
-void ObjYami::Init()
+void ObjCapture::Init()
 {
 
 }
 
 //アクション
-void ObjYami::Action()
+void ObjCapture::Action()
 {
 
 }
 
-void ObjYami::Draw()
+void ObjCapture::Draw()
 {
 	//描画カラー情報　R=Red　G=Green　B=Blue　A=alpha(透過情報)
 	float c[4] = { 1.0f,1.0f,1.0f,1.0f, };
@@ -41,11 +41,7 @@ void ObjYami::Draw()
 	dst.m_right = 800.0f;
 	dst.m_bottom = 600.0f;
 
-	if (m_num == 1) { m_num = 15; }
-	else if (m_num == 2) { m_num = 16; }
-	else if (m_num == 3) { m_num = 17; }
-	else { ; }
-
+	//作成時受け取ったm_numの数をそのまま画像番号に入れる
 	//描画
 	Draw::Draw(m_num, &src, &dst, c, 0.0f);
 
---
+++ 桃太郎最強伝説/桃太郎最強伝説/SceneSell.h
@@ -0,0 +1,19 @@
+#pragma once
+//使用するヘッダーファイル
+#include "GameL\SceneManager.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//シーン：ゲームメイン
+class CSceneSell :public CScene
+{
+public:
+	CSceneSell();
+	~CSceneSell();
+	void InitScene();	//ゲームメインの初期化メソッド
+	void Scene();		//ゲームメインの実行中メソッド
+private:
+	int m_time;
+};
+
