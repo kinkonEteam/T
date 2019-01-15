0d38bafb39a6171758b3c63e400443e52a173c6a
中宇地 翔平 <GA1@DEV1-1>
2019-01-15 14:43:53 +0900

    変更

--- 桃太郎最強伝説/桃太郎最強伝説/GameHead.h
+++ 桃太郎最強伝説/桃太郎最強伝説/GameHead.h
@@ -164,5 +164,5 @@ enum ITEM
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START  CSceneTitle
+#define SET_GAME_START  CSceneClear
 //-----------------------------------------------
